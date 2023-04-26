b3255a75c1fa00e0ff7ea5b3d64caf892ec0f675
hgivtujcgjv <112762784+hgivtujcgjv@users.noreply.github.com>
2023-04-26 23:28:03 +0300

    Update test_acc.cpp
    арпв

--- tests/test_acc.cpp
+++ tests/test_acc.cpp
@@ -3,52 +3,44 @@
 #include "gtest.hpp"
 
 
-TEST(AccountTest, GetBalance) {
-    MockAccount account(1, 100);
-    EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(100));
-    EXPECT_EQ(account.GetBalance(), 100);
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "gtest.hpp"
+
+
+TEST(MockAccount, Getter) {
+    MockAccount account(1,10);
+    EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(10));
+    EXPECT_EQ(10, account.GetBalance());
 }
 
-
-/// Test `Account::ChangeBalance()`
-/// Should throw exception because account is not locked
-TEST(AccountTest, ChangeBalanceException) {
-    MockAccount account(1, 100);
-    EXPECT_CALL(account, ChangeBalance(100)).WillOnce(testing::Throw(std::runtime_error("at first lock the account")));
-    EXPECT_THROW(account.ChangeBalance(100), std::runtime_error);
-}
-
-/// Test `Account::ChangeBalance()`
-/// But first lock the account
-TEST(AccountTest, ChangeBalance) {
-    MockAccount account(1, 100);
+TEST(MockAccount, ChangeBalance) {
+    MockAccount account(1,10);
     EXPECT_CALL(account, Lock());
+    EXPECT_CALL(account, ChangeBalance(5));
+    EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(15));
     account.Lock();
-    EXPECT_CALL(account, ChangeBalance(100));
-    account.ChangeBalance(100);
-
-    EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(200));
-    EXPECT_EQ(account.GetBalance(), 200);
+    account.ChangeBalance(5);
+    EXPECT_EQ(15, account.GetBalance());
 }
 
-
-/// Test `Account::Lock()`
-TEST(AccountTest, Lock) {
-    MockAccount account(1, 100);
-    EXPECT_CALL(account, Lock());
+TEST(MockAccount, Lock) {
+    MockAccount account(1,10);
+    EXPECT_CALL(account, Lock()).Times(2).WillOnce(testing::Return()).WillOnce(testing::Throw(std::runtime_error("Already locked")));
     account.Lock();
-}
-
-/// Test throwing exception from `Account::Lock()` if account is already locked
-TEST(AccountTest, LockException) {
-    MockAccount account(1, 100);
-    EXPECT_CALL(account, Lock()).WillOnce(testing::Throw(std::runtime_error("already locked")));
     EXPECT_THROW(account.Lock(), std::runtime_error);
 }
-
-/// Test `Account::Unlock()`
-TEST(AccountTest, Unlock) {
-    MockAccount account(1, 100);
+TEST(MockAccount, Unlock) {
+    MockAccount account(1,10);
+    EXPECT_CALL(account, Lock());
+    EXPECT_CALL(account, ChangeBalance(5));
     EXPECT_CALL(account, Unlock());
+    EXPECT_CALL(account, ChangeBalance(-5));
+    EXPECT_CALL(account, GetBalance()).WillOnce(testing::Return(10));
+    account.Lock();
+    account.ChangeBalance(5);
     account.Unlock();
+    account.ChangeBalance(-5);
+    EXPECT_EQ(10, account.GetBalance());
 }
+
