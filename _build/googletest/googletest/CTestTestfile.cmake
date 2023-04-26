687f55cf0c449f9f4026b00aca1a50cfb74f64ab
hgivtujcgjv <112762784+hgivtujcgjv@users.noreply.github.com>
2023-04-26 21:31:34 +0300

    Delete _build directory

--- _build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user1111/lab5555/googletest/googletest
-# Build directory: /home/user1111/lab5555/_build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
